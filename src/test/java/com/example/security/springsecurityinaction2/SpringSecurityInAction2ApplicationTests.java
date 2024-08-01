c1ee134b89446f80e061cae420096761a7b1302b
DongWoonKim <01030985843z@naver.com>
2024-08-01 14:50:04 +0900

    prj setup

---
+++ src/test/java/com/example/security/springsecurityinaction2/SpringSecurityInAction2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security.springsecurityinaction2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInAction2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
